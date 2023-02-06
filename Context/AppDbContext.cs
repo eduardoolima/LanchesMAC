62f12ce2fbda0cbb253a131baf54480f1ea22300
Eduardo <eduardo.oliveira@legalcontrol.com.br>
2023-02-06 05:46:23 -0300

    definida classe de contexto

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Snack> Snacks { get; set; }
+    }
+}
