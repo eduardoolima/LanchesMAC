988beeee3ee1e3d21bd6631a1092bb89c08dca16
Eduardo <eduardo.oliveira@legalcontrol.com.br>
2023-02-06 04:56:07 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Snack.cs
@@ -0,0 +1,18 @@
+﻿namespace LanchesMac.Models
+{
+    public class Snack
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SmallDescription { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImgPath { get; set; }
+        public string ImgThumbnailPath { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool IsAvaible { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
